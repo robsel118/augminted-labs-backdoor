382f76a244a2bba1f82839542998407e4dfd5fbd
robsel118 <robert.seligmann@protonmail.com>
2022-03-08 19:53:07 +0700

    feat(tailwind): added plugin
    added tailwind plugin for forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,43 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundImage: {
-      sorashima: 'url("/src/assets/sorashima.gif")',
-      'gradient-black': 'linear-gradient(180deg,#000 0,#000 15vh,rgba(0,0,0,.212) 80vh,rgba(255,0,0,0) 100vh)'
-    },
     extend: {
-      colors: {
-        "red": "#ed3823"
+      backgroundImage: {
+        sorashima: 'url("/src/assets/sorashima.gif")',
+        'gradient-black': 'linear-gradient(180deg,#000 0,#000 15vh,rgba(0,0,0,.212) 80vh,rgba(255,0,0,0) 100vh)'
       },
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(0.5deg)',
+          },
+          '75%': {
+            transform: 'rotate(-0.5deg)',
+          },
+        },
+      },
+      fontFamily: {
+        glitch: 'Hacked, serif'
+      },
+      colors: {
+        "redioactive": "#ed3823",
+        "green-corrosive": "#2bf586"
+      },
+      backgroundColor: {
+        panel: 'rgba(42,39,53,0.75)'
+      }
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
