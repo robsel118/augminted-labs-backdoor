8ccc3a51cd5f4e610e6167e8408d3dfb287553ae
robsel118 <robert.seligmann@protonmail.ch>
2022-03-15 23:17:22 +0700

    feat(page): added wallet page with nft viewer component

--- src/containers/NftViewer.tsx
+++ src/containers/NftViewer.tsx
@@ -4,34 +4,12 @@ import { KAIJU_KINGZ_ADDRESS, MUTANTS_ADDRESS } from 'src/config/constants'
 import { useKaijuContract } from 'src/hooks/useKaijuContract'
 import { useMutantContract } from 'src/hooks/useMutantContract'
 import { Nft } from 'src/interfaces/nft.interface'
+import { UseNFTBalancesParams } from 'react-moralis'
 
 export const NftViewer: FC<{ wallet: string }> = ({ wallet }) => {
   const { data } = useNFTBalances({ chain: 'eth', address: wallet }, { autoFetch: true })
-  const { getKaijuBalanceMetadata } = useKaijuContract()
-  const { getMutantBalanceMetadata } = useMutantContract()
   const [kaijus, setKaijus] = useState<Nft[]>([])
   const [mutants, setMutants] = useState<Nft[]>([])
-
-  useEffect(() => {
-    if (data?.result) {
-      console.log(data)
-      const kaijuIds = data.result
-        .filter((nft) => nft.token_address === KAIJU_KINGZ_ADDRESS)
-        .map((nft) => nft.token_id)
-      const mutantIds = data.result
-        .filter((nft) => nft.token_address === MUTANTS_ADDRESS)
-        .map((nft) => nft.token_id)
-      console.log(kaijuIds)
-
-      getKaijuBalanceMetadata(kaijuIds).then((res) => {
-        setKaijus(res.map((kaiju) => kaiju.data))
-      })
-
-      getMutantBalanceMetadata(mutantIds).then((res) => {
-        setMutants(res.map((mutant) => mutant.data))
-      })
-    }
-  }, [data?.result])
 
   return (
     <div>
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DataLoader } from 'src/containers/DataLoader'
+import { Nft } from 'src/interfaces/nft.interface'
+import { RwasteBalance } from 'src/interfaces/RwasteBalance.interface'
 
 export const WalletPage = () => {
-  return <DataLoader />
+  const [kaijus, setKaijus] = useState<Nft[]>([])
+  const [mutants, setMutants] = useState<Nft[]>([])
+  const [rwaste, setRwaste] = useState<RwasteBalance>({ held: 0, toClaim: 0 })
+  return <DataLoader setKaijus={setKaijus} setMutants={setMutants} setRwaste={setRwaste} />
 }
